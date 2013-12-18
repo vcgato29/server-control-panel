9e24873fa612ab98f03b18c67742db071deb00ef
jakoch <jakoch@web.de>
2013-12-18 14:16:07 +0100

    added license header

--- src/windowsapi.h
+++ src/windowsapi.h
@@ -1,3 +1,26 @@
+/*
+    WPN-XM Server Control Panel
+
+    WPN-XM SCP is a GUI tool for managing server daemons under Windows.
+    It's a fork of Easy WEMP written by Yann Le Moigne and (c) 2010.
+    WPN-XM SCP is written by Jens-Andre Koch and (c) 2011 - onwards.
+
+    This file is part of WPN-XM Server Stack for Windows.
+
+    WPN-XM SCP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WPN-XM SCP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WPN-XM SCP. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef WINDOWSAPI_H
 #define WINDOWSAPI_H
 
