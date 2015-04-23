7d577b5fc998ce5398f1f5260b603e2790a73766
jakoch <jakoch@web.de>
2015-04-23 21:43:59 +0200

    read current tag version from build.xml and insert it into the download links automatically [skip ci]

--- bin/build-boxed-scp.php
+++ bin/build-boxed-scp.php
@@ -18,25 +18,44 @@
 
 // wpnxm-scp x86
 
-download('https://github.com/WPN-XM/server-control-panel/releases/download/0.8.1/wpnxm-scp-x86.zip', 'wpnxm-scp-x86.zip');
+download('https://github.com/WPN-XM/server-control-panel/releases/download/'. getVersion() . '/wpnxm-scp-x86.zip', 'wpnxm-scp-x86.zip');
 unzip('wpnxm-scp-x86.zip', __DIR__ . '/wpnxm-scp-x86');
 passthru('php EnigmaVirtualBox.php wpnxm-scp-x86 wpn-xm.exe wpn-xm-x86_boxed.exe');
 rename(__DIR__ . '\wpn-xm-x86_boxed.exe', __DIR__ . '\wpn-xm.exe');
-passthru(__DIR__ . '\7z\7z.exe a -tzip wpnxm-scp-x86_boxed.zip wpn-xm.exe -mx9 -mmt');
+passthru(__DIR__ . '\7z\7za.exe a -tzip wpnxm-scp-x86_boxed.zip wpn-xm.exe -mx9 -mmt');
 unlink(__DIR__ . '\wpn-xm.exe');
 unlink(__DIR__ . '\wpnxm-scp-x86.zip');
 unlink(__DIR__ . '\project.evb');
 
 // wpnxm-scp x86_64
 
-download('https://github.com/WPN-XM/server-control-panel/releases/download/0.8.1/wpnxm-scp-x86_64.zip', 'wpnxm-scp-x86_64.zip');
+download('https://github.com/WPN-XM/server-control-panel/releases/download/'. getVersion() . '/wpnxm-scp-x86_64.zip', 'wpnxm-scp-x86_64.zip');
 unzip('wpnxm-scp-x86_64.zip', __DIR__ . '/wpnxm-scp-x86_64');
 passthru('php EnigmaVirtualBox.php wpnxm-scp-x86_64 wpn-xm.exe wpn-xm-x86_64_boxed.exe');
 rename(__DIR__ . '\wpn-xm-x86_64_boxed.exe', __DIR__ . '\wpn-xm.exe');
-passthru(__DIR__ . '\7z\7z.exe a -tzip wpnxm-scp-x86_64_boxed.zip wpn-xm.exe -mx9 -mmt');
+passthru(__DIR__ . '\7z\7za.exe a -tzip wpnxm-scp-x86_64_boxed.zip wpn-xm.exe -mx9 -mmt');
 unlink(__DIR__ . '\wpn-xm.exe');
 unlink(__DIR__ . '\wpnxm-scp-x86_64.zip');
 unlink(__DIR__ . '\project.evb');
+
+function xml2array($xmlString)
+{
+	$xml = simplexml_load_string($xmlString, 'SimpleXMLElement', LIBXML_NOCDATA);
+	return json_decode(json_encode((array)$xml), TRUE);
+}
+
+function getVersion()
+{
+	$buildfile = dirname(__DIR__) . '/build.xml';
+	$content = file_get_contents($buildfile);
+	$array = xml2array($content);
+
+	$version = $array['property'][0]['@attributes']['value'] . '.' .
+	           $array['property'][1]['@attributes']['value'] . '.' .
+	           $array['property'][2]['@attributes']['value'];
+
+	return $version;
+}
 
 function unzip($zipfile, $folder)
 {
