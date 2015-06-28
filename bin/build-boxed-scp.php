2688a748e6be0f867176ff2642e2212d96686134
jakoch <jakoch@web.de>
2015-06-28 13:03:12 +0200

    tweaked scp boxing script to download and create versionized zip files

--- bin/build-boxed-scp.php
+++ bin/build-boxed-scp.php
@@ -8,84 +8,88 @@
  * For full copyright and license information, view the bundled LICENSE file.
  */
 
+$version = getVersion();
+
+repackage($version, 'x86');
+repackage($version, 'x86_64');
+
 /**
+ * Repackage
+ *
  * 1. download the latest version of the server control panel
  * 2. unzip
- * 3. use EngimaVirtualBox helper to
- *    - build a evb project from the folder
- *    - "box" the application (package into a single executable)
+ * 3. use EngimaVirtualBox helper script to box the Qt application
+ *    - build an EVB project from the unzipped application folder
+ *    - "box" the Qt application: result is a single executable.
+ * 4. zip the executable (the file suffix is "_boxed.zip").
+ * 5. cleanup
  */
-
-// wpnxm-scp x86
-
-download('https://github.com/WPN-XM/server-control-panel/releases/download/'. getVersion() . '/wpnxm-scp-x86.zip', 'wpnxm-scp-x86.zip');
-unzip('wpnxm-scp-x86.zip', __DIR__ . '/wpnxm-scp-x86');
-passthru('php EnigmaVirtualBox.php wpnxm-scp-x86 wpn-xm.exe wpn-xm-x86_boxed.exe');
-rename(__DIR__ . '\wpn-xm-x86_boxed.exe', __DIR__ . '\wpn-xm.exe');
-passthru(__DIR__ . '\7z\7za.exe a -tzip wpnxm-scp-x86_boxed.zip wpn-xm.exe -mx9 -mmt');
-unlink(__DIR__ . '\wpn-xm.exe');
-unlink(__DIR__ . '\wpnxm-scp-x86.zip');
-unlink(__DIR__ . '\project.evb');
-
-// wpnxm-scp x86_64
-
-download('https://github.com/WPN-XM/server-control-panel/releases/download/'. getVersion() . '/wpnxm-scp-x86_64.zip', 'wpnxm-scp-x86_64.zip');
-unzip('wpnxm-scp-x86_64.zip', __DIR__ . '/wpnxm-scp-x86_64');
-passthru('php EnigmaVirtualBox.php wpnxm-scp-x86_64 wpn-xm.exe wpn-xm-x86_64_boxed.exe');
-rename(__DIR__ . '\wpn-xm-x86_64_boxed.exe', __DIR__ . '\wpn-xm.exe');
-passthru(__DIR__ . '\7z\7za.exe a -tzip wpnxm-scp-x86_64_boxed.zip wpn-xm.exe -mx9 -mmt');
-unlink(__DIR__ . '\wpn-xm.exe');
-unlink(__DIR__ . '\wpnxm-scp-x86_64.zip');
-unlink(__DIR__ . '\project.evb');
+function repackage($version, $bitsize) '. $bitsize .'
+{
+    // 1
+    download('https://github.com/WPN-XM/server-control-panel/releases/download/'. $version . '/wpnxm-scp-'. $version . '-'. $bitsize .'.zip', 'wpnxm-scp-'. $bitsize .'.zip');
+    // 2
+    unzip('wpnxm-scp-'. $bitsize .'.zip', __DIR__ . '/wpnxm-scp-'. $bitsize);
+    // 3
+    passthru('php EnigmaVirtualBox.php wpnxm-scp-'. $bitsize .' wpn-xm.exe wpn-xm-'. $bitsize .'_boxed.exe');
+    rename(__DIR__ . '\wpn-xm-'. $bitsize .'_boxed.exe', __DIR__ . '\wpn-xm.exe');
+    // 4
+    passthru(__DIR__ . '\7z\7za.exe a -tzip wpnxm-scp-'. $version . '-'. $bitsize .'_boxed.zip wpn-xm.exe -mx9 -mmt');
+    // 5
+    unlink(__DIR__ . '\wpn-xm.exe');
+    unlink(__DIR__ . '\wpnxm-scp-'. $bitsize .'.zip');
+    unlink(__DIR__ . '\project.evb');
+}
 
 function xml2array($xmlString)
 {
-	$xml = simplexml_load_string($xmlString, 'SimpleXMLElement', LIBXML_NOCDATA);
-	return json_decode(json_encode((array)$xml), TRUE);
+    $xml = simplexml_load_string($xmlString, 'SimpleXMLElement', LIBXML_NOCDATA);
+
+    return json_decode(json_encode((array)$xml), TRUE);
 }
 
 function getVersion()
 {
-	$buildfile = dirname(__DIR__) . '/build.xml';
-	$content = file_get_contents($buildfile);
-	$array = xml2array($content);
+    $buildfile = dirname(__DIR__) . '/build.xml';
+    $content = file_get_contents($buildfile);
+    $array = xml2array($content);
 
-	$version = $array['property'][0]['@attributes']['value'] . '.' .
-	           $array['property'][1]['@attributes']['value'] . '.' .
-	           $array['property'][2]['@attributes']['value'];
+    $version = $array['property'][0]['@attributes']['value'] . '.' .
+               $array['property'][1]['@attributes']['value'] . '.' .
+               $array['property'][2]['@attributes']['value'];
 
-	return $version;
+    return $version;
 }
 
 function unzip($zipfile, $folder)
 {
-	$zip = new ZipArchive;
-	$res = $zip->open($zipfile);
-	if ($res === TRUE) {
-	  	$zip->extractTo($folder);
-	  	$zip->close();
-	}
+    $zip = new ZipArchive;
+    $res = $zip->open($zipfile);
+    if ($res === TRUE) {
+        $zip->extractTo($folder);
+        $zip->close();
+    }
 }
 
 function download($url, $targetFile)
 {
-	$file = fopen($url, "rb");
+    $file = fopen($url, "rb");
 
-	if ($file) {
-		$newf = fopen($targetFile, "wb");
+    if ($file) {
+        $newf = fopen($targetFile, "wb");
 
-		if ($newf) {
-			while(!feof($file)) {
-				fwrite($newf, fread($file, 1024 * 8), 1024 * 8);
-			}
-		}
-	}
+        if ($newf) {
+            while(!feof($file)) {
+                fwrite($newf, fread($file, 1024 * 8), 1024 * 8);
+            }
+        }
+    }
 
-	if ($file) {
-		fclose($file);
-	}
+    if ($file) {
+        fclose($file);
+    }
 
-	if ($newf) {
-		fclose($newf);
-	}
+    if ($newf) {
+        fclose($newf);
+    }
  }
